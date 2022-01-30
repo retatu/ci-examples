71ca58a6bdbc57818128f25097ba635bac9a90a9
Matheus <mrehbein45@gmail.com>
2022-01-30 10:45:58 -0300

    fix: :bug: fix test that wasn't passing

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSome(t *testing.T) {
-	total := Soma(15, 10)
+	total := soma(15, 10)
 
 	if total != 25 {
 		t.Errorf("Resultado Soma inválido. Result: %d Expected: %d", total, 25)
