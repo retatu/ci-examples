b4779fd5d89c2f493561edef6be96943c2301749
Matheus <mrehbein45@gmail.com>
2022-02-01 06:36:28 -0300

    test: :white_check_mark: add multiplication test

--- math_test.go
+++ math_test.go
@@ -2,8 +2,16 @@ package main
 
 import "testing"
 
-func TestSome(t *testing.T) {
+func TestSoma(t *testing.T) {
 	total := soma(15, 10)
+
+	if total != 25 {
+		t.Errorf("Resultado Soma inválido. Result: %d Expected: %d", total, 25)
+	}
+
+}
+func TestMulti(t *testing.T) {
+	total := multi(5, 5)
 
 	if total != 25 {
 		t.Errorf("Resultado Soma inválido. Result: %d Expected: %d", total, 25)
